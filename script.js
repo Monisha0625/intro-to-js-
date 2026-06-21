9aed5e9f088b92e5ed7ef3e50dd878f6807e02d4
Monisha0625 <monisha2007t@gmail.com>
2026-06-21 19:49:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let doraAge = 10;
+let bestFriendName = "Buji";
+
+console.log("Hi! I'm Dora, and my best friend is " + bestFriendName + ". I am " + doraAge + " years old!");
+
+let allowance = 500;
+let game = 150;
+let fries = 100;
+let savings = 50;
+
+let remainingBalance = allowance - game - fries - savings;
+
+console.log("Ben has $" + remainingBalance + " left after his expenses.");
+
+let bheemAge = 14;
+
+console.log("Chhota Bheem eligible to buy super-sized laddus: " + (bheemAge >= 12));
+
+let friendName = "Jack";
+let partyLocation = "Oggy's house";
+
+console.log("Hey " + friendName + ", you're invited to " + partyLocation + " for the party!");
+
+let totalCheese = 1200;
+let people = 2;
+
+console.log("Tom & Jerry gets " + (totalCheese / people) + " pieces of cheese each.");
+console.log("Leftover cheese: " + (totalCheese % people) + " pieces.");
